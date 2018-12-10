12bf3b220ab2ef3e7b4308ac8ccb8c97cef7de36
Omid Azodi <azodi002@users.noreply.github.com>
2018-12-09 17:22:37 -0800

    test

--- RodeosCateringProject/RodeosCateringProject/ViewController.swift
+++ RodeosCateringProject/RodeosCateringProject/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var menu_btn: UIButton!
+    @IBOutlet weak var place_order_btn: UIButton!
+    @IBOutlet weak var contact_us_btn: UIButton!
+    
+    
+    @IBAction func place_order_pressed(_ sender: Any) {
+        performSegue(withIdentifier: "place_order", sender: self)
+    }
+    
+    override func viewDidLoad()
+    {
+        FirebaseApp.configure()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        menu_btn.layer.cornerRadius = 15
+        place_order_btn.layer.cornerRadius = 15
+        contact_us_btn.layer.cornerRadius = 15
     }
-
-
 }
 
